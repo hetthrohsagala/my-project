c6baeace3dfc45b3b83ab958f4e8093d732c9647
hetthrohsagala <hetthrohsagala@gmail.com>
2018-05-31 20:06:02 +0700

    Table sql kulina

---
+++ table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `agenda`.`user_review` ( `id` INT NOT NULL AUTO_INCREMENT , `order_id` INT NOT NULL , `product_id` INT NOT NULL , `user_id` INT NOT NULL , `rating` FLOAT(1,2) NOT NULL , `review` VARCHAR NOT NULL , `created_at` TIMESTAMP NOT NULL , `updated_at` TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
